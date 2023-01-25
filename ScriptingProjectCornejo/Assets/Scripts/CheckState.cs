04bef8468146641d8325f806b781d03ba3c586ca
heissy <heisycornejo@gmail.com>
2023-01-25 09:18:40 -0800

    Added New Script
    I added a new script to my projecting.

---
+++ ScriptingProjectCornejo/Assets/Scripts/CheckState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckState : MonoBehaviour
+{
+    public GameObject myObject;
+    
+
+    void Start()
+    {
+        Debug.Log("Active Self: " + myObject.activeSelf);
+        Debug.Log("Active in Hierarchy" + myObject.activeInHierarchy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
