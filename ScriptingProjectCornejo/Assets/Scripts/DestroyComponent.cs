17191e7045ab470942fa07d243b0e7ad41ff0ed8
heissy <heisycornejo@gmail.com>
2023-03-07 09:52:03 -0800

    Added a new script.
    Added the script get axis to project.

--- ScriptingProjectCornejo/Assets/Scripts/DestroyComponent.cs
+++ ScriptingProjectCornejo/Assets/Scripts/DestroyComponent.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WhileLoop : MonoBehaviour
+public class DestroyComponent : MonoBehaviour
 {
-    int cupsInTheSink = 4;
     // Start is called before the first frame update
     void Start()
     {
-        while(cupsInTheSink > 0)
-        {
-            Debug.Log("I've washed a cup!");
-            cupsInTheSink--;
-        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(GetComponent<MeshRenderer>());
+        }
     }
 }
--- ScriptingProjectCornejo/Assets/Scripts/DestroyOther.cs
+++ ScriptingProjectCornejo/Assets/Scripts/DestroyOther.cs
@@ -2,24 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoWhileLoop : MonoBehaviour
+public class DestroyOther : MonoBehaviour
 {
+    public GameObject other;
     // Start is called before the first frame update
     void Start()
     {
-        bool shouldContinue = false;
-
-        do
-        {
-            print("Hello World");
-
-        }while (shouldContinue == true);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(other);
+        }
     }
 }
